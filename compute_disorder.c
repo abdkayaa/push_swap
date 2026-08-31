dc8038fed6a46294fd0384c3df7d66e71ae2aeca
abdkayaa <82145062+abdkayaa@users.noreply.github.com>
2026-08-31 10:35:32 +0000

    compute_disorder functions finish

---
+++ compute_disorder.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   compute_disorder.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abdkaya@student.42istanbul.com.tr          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/31 07:02:02 by abdkaya           #+#    #+#             */
+/*   Updated: 2026/08/31 10:15:21 by abdkaya          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	is_sorted(int *stack_a, int size)
+{
+	int	i;
+
+	if (size <= 1)
+		return (1);
+	i = 0;
+	while (i < size - 1)
+	{
+		if (stack_a[i] > stack_a[i + 1])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+double	compute_disorder(int *stack_a, int size)
+{
+	long long	mistakes;
+	long long	total_pairs;
+	int			i;
+	int			j;
+
+	if (size <= 1)
+		return (0, 0);
+	mistakes = 0;
+	total_pairs = ((long long)size * (size - 1)) / 2;
+	i = 0;
+	while (i < size - 1)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (stack_a[i] > stack_a[j])
+				mistakes++;
+			j++;
+		}
+		i++;
+	}
+	return ((double)mistakes / (double)total_pairs);
+}
