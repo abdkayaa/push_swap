6647f2c24ff7583294f0711c76de8a3fea7cf31a
iamch3f <iamch3f@users.noreply.github.com>
2026-09-13 00:04:15 +0300

    Implement index compression function in sort_medium.

--- sort_medium.c
+++ sort_medium.c
@@ -6,8 +6,25 @@
 /*   By: abdkaya <abdkaya@student.42istanbul.com.tr>  +#+  +#+       +#+      */
 /*                                                  +#+#+#+#+#+   +#+         */
 /*   Created: 2026/09/08 14:23:17 by abdkaya             #+#    #+#           */
-/*   Updated: 2026/09/08 14:23:19 by abdkaya            ###   ########.fr     */
+/*   Updated: 2026/09/12 22:21:21 by abdkaya            ###   ########.fr     */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pushswap.h"
+
+static void index_compress(t_stack *a)
+{
+	int	i;
+	int	count;
+	int	*tmp;
+
+	i = 0;
+	tmp = malloc(n * sizeof(int));
+	if (!tmp)
+		return (NULL);
+	while(a->size)	
+	{
+		count = 0;
+		while() 
+	}	
+}
